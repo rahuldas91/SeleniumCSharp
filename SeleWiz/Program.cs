4f9dab0bf7b68d1f5a08e329e6f4c84ee3a8400d
LAPTOP-3GGOEH9Q\RAHUL DAS <rahuldas90@gmail.com>
2020-08-14 17:12:13 +0530

    Extent Report 3.1.3 added . implemented basic reporting mechanism for trying out. need to add classes for code organization

--- SeleWiz/Program.cs
+++ SeleWiz/Program.cs
@@ -3,6 +3,11 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using SeleWiz.PageObjects;
+using System.Windows.Forms;
+using AventStack.ExtentReports;
+using System.IO;
+using AventStack.ExtentReports.Reporter;
+using OpenQA.Selenium;
 
 namespace SeleWiz
 {
@@ -10,21 +15,33 @@ namespace SeleWiz
     {
         Login objLogin;
         Dictionary<string, string> dictionary;
-     [OneTimeSetUp]
+        public ExtentReports _extent;
+        public ExtentTest _test;
+
+
+        [OneTimeSetUp]
         public void oneTimeSetup()
         {
 
-           dictionary =
-                       new Dictionary<string, string>();
+            dictionary = new Dictionary<string, string>();
 
             dictionary.Add("username", "SampleUser");
             dictionary.Add("password", "DMyPasswordas");
 
-            //foreach (KeyValuePair<string, string> ele2 in dictionary)
-            //{
 
-            //    Debug.WriteLine("{0} and {1}", ele2.Key, ele2.Value);
-            //}
+
+            var path = System.Reflection.Assembly.GetCallingAssembly().CodeBase;
+            var actualPath = path.Substring(0, path.LastIndexOf("bin"));
+            var projectPath = new Uri(actualPath).LocalPath;
+            Directory.CreateDirectory(projectPath.ToString() + "Reports");
+            var reportPath = projectPath + "Reports\\ExtentReport.html";
+            var htmlReporter = new ExtentHtmlReporter(reportPath);
+            _extent = new ExtentReports();
+            _extent.AttachReporter(htmlReporter);
+            _extent.AddSystemInfo("Host Name", "LocalHost");
+            _extent.AddSystemInfo("Environment", "QA");
+            _extent.AddSystemInfo("UserName", "TestUser");
+
         }
 
         [OneTimeTearDown]
@@ -38,9 +55,7 @@ namespace SeleWiz
         {
             DriverManager.Instance.startBrowser();
             DriverManager.Instance.driver.Url = "https://www.facebook.com/";
-            // dm.driver.Url = "http://www.google.co.in";
 
-            //driver = new ChromeDriver(@"E:\Work\VisualStudio\SeleWiz\SeleWiz\3rdParty");
         }
 
         [Test]
@@ -49,12 +64,56 @@ namespace SeleWiz
             objLogin = new Login();
             objLogin.enterUserName(dictionary["username"]);
             objLogin.enterpassword(dictionary["password"]);
+
+            String pageTitle = DriverManager.Instance.driver.Title;
+            //MessageBox.Show(pageTitle);
+            //Assert.Equals("Facebook – log in or sign up", pageTitle);
+            _test = _extent.CreateTest(TestContext.CurrentContext.Test.Name);
+            Assert.IsTrue(pageTitle.Contains("Facebook"));
+
+            _test.Log(Status.Fail, "Fail");
+
+            DateTime time = DateTime.Now;
+            String fileName = "Screenshot_" +time.ToString("h_mm_ss") + ".png";
+            String screenShotPath = Capture(DriverManager.Instance.driver, fileName);
+            _test.Log(Status.Fail, "Snapshot below: " +_test.AddScreenCaptureFromPath("Screenshots\\" +fileName));
+
+
+            _test = _extent.CreateTest(TestContext.CurrentContext.Test.Name);
+            Assert.IsTrue(pageTitle.Contains("Facebook"));
+
+            _test.Log(Status.Pass,"THis is skipped");
+
+            time = DateTime.Now;
+            fileName = "Screenshot_" + time.ToString("h_mm_ss") + ".png";
+            screenShotPath = Capture(DriverManager.Instance.driver, fileName);
+            _test.Log(Status.Skip, "Snapshot below: " + _test.AddScreenCaptureFromPath("Screenshots\\" + fileName));
+
+
         }
 
         [TearDown]
         public void closeBrowser()
         {
-            //DriverManager.Instance.driver.Close();
+            DriverManager.Instance.driver.Close();
+            _extent.Flush();
         }
+
+
+        public static string Capture(IWebDriver driver, String screenShotName)
+        {
+            ITakesScreenshot ts = (ITakesScreenshot)driver;
+            Screenshot screenshot = ts.GetScreenshot();
+            var pth = System.Reflection.Assembly.GetCallingAssembly().CodeBase;
+            var actualPath = pth.Substring(0, pth.LastIndexOf("bin"));
+            var reportPath = new Uri(actualPath).LocalPath;
+            Directory.CreateDirectory(reportPath + "Reports\\" + "Screenshots");
+            var finalpth = pth.Substring(0, pth.LastIndexOf("bin")) + "Reports\\Screenshots\\" +screenShotName;
+            var localpath = new Uri(finalpth).LocalPath;
+            screenshot.SaveAsFile(localpath, ScreenshotImageFormat.Png);
+            return reportPath;
+        }
+
     }
+    
 }
